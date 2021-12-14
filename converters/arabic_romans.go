9d26819275b8f98497d290710553fd1e82e351bd
Julio Rodriguez <julioalfonso8003@gmail.com>
2021-12-14 18:21:26 -0500

    Working..

--- converters/arabic_romans.go
+++ converters/arabic_romans.go
@@ -1,9 +1,26 @@
 package converters
 
+import "fmt"
+
+/*
+I->1
+V->5
+X->10
+L->50
+C->100
+D->500
+M->1000
+*/
+
 var ConstArabicToRomans = map[string]uint8{
 	"I": 1,
 }
 
-func ArabicToRomans() {
-
+func ArabicToRomans(number uint16) {
+	var digitos []uint16
+	for i := number; i <= 0; {
+		i /= 10
+		digitos = append(digitos, i)
+	}
+	fmt.Println(digitos)
 }
